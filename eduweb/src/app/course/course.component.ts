385d093a5c73806ec7be12091cef6ca93fde42ec
Shibin Hussain <shibinhussain82@gmail.com>
2021-07-20 13:43:52 +0530

    course commit

--- eduweb/src/app/course/course.component.ts
+++ eduweb/src/app/course/course.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
 })
 export class CourseComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  course = [
+    {
+      id: 1,
+      name: 'Learn Angular',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor consectetur adipiscing elit, sed do eiusmod tempor incididunt ncididunt ut labore et dolore magna aliqua',
+      image: '../../assets/angular.png',
+    },
+    {
+      id: 2,
+      name: 'Learn Javascript',
+      description:
+        'Lorem ipsum dolor sit amet, dolore magna aliqua., consectett, consectetur adipiscing elit, sed do consectetur adipiscing elit, s ',
+      image: '../../assets/js.png',
+    },
+    {
+      id: 3,
+      name: 'Learn Typescript',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do consectetur sed do eiusmod tempor incididunt ncididunt ut labore et dolore magna aliqua.',
+      image: '../../assets/ts.jpg',
+    },
+    {
+      id: 4,
+      name: 'Learn Python',
+      description:
+        'Lorem ipsum dolor sit, consectetur adipiscing elit, sed do eiusmod tempor tempor tempor adipiscing elit, sed do eiusmod tempor incididunt ncididunt ut labore et dolore magna aliqua.',
+      image: '../../assets/python.jpg',
+    },
+  ];
 }
